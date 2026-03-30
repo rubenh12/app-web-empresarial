1dd4b35acd5eecce2201522d1010175af1365a78
rubenh12 <herreramorales910@gmail.com>
2026-03-30 10:12:13 -0600

    agregar json al endpoint raiz

--- servidor/src/app.controller.ts
+++ servidor/src/app.controller.ts
@@ -3,10 +3,13 @@ import { AppService } from './app.service.js';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      status: 'success',
+      message: 'API - Servidor Activo'
+    };
   }
 }
